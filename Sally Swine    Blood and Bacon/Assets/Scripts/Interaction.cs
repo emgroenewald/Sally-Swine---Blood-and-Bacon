69db9908b20c869c118e1c7d43bb9713cec1efa4
emgroenewald <elliot.mills-groenewald@elev.ga.lbs.se>
2024-02-05 20:04:02 +0100

    Interaction Finished
    Finished the interaction system with some simple tweaks, aswell as fixing up the living room sprites

--- Sally Swine    Blood and Bacon/Assets/Scripts/Interaction.cs
+++ Sally Swine    Blood and Bacon/Assets/Scripts/Interaction.cs
@@ -14,9 +14,30 @@ public class NewBehaviourScript : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (IsinRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            IsinRange = true;
+            Debug.Log("Player now in range");
+        }
+    }
+
+    private void nTriggerExit(Collider2D other)
+    {
+        IsinRange = false;
+            Debug.Log("Player now not in range");
     }
 }
