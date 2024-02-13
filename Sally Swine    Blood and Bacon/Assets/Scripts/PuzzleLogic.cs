8942e7160b099287fc499414ed33373dce73d056
emgroenewald <elliot.mills-groenewald@elev.ga.lbs.se>
2024-02-13 12:02:58 +0100

    PuzzleLogic

--- Sally Swine    Blood and Bacon/Assets/Scripts/PuzzleLogic.cs
+++ Sally Swine    Blood and Bacon/Assets/Scripts/PuzzleLogic.cs
@@ -1,90 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PuzzleLogic : MonoBehaviour
 {
-    [SerializeField] private bool FirstPuzzle = false;
-    [SerializeField] private bool SecondPuzzle = false;
-    [SerializeField] private bool ThirdPuzzle = false;
-
-    // Variables to store the correct colors for each slot
-    [SerializeField] private string correctColorForFirstSlot = "Red";
-    [SerializeField] private string correctColorForSecondSlot = "Green";
-    [SerializeField] private string correctColorForThirdSlot = "Blue";
-
+    [SerializeField] string expectedColor;
+    [SerializeField] string slot;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        string slotTag = collision.tag;
-
-      
-        switch (slotTag)
+        if (gameObject.CompareTag("expectedColor"))
         {
-            case "FirstSlot":
-                HandleFirstSlot(collision);
-                break;
-            case "SecondSlot":
-                HandleSecondSlot(collision);
-                break;
-            case "ThirdSlot":
-                HandleThirdSlot(collision);
-                break;
-        }
-    }
-
-    private void HandleFirstSlot(Collider2D collision)
-    {
-        string enteredColor = collision.transform.tag;
-
-        if (enteredColor == correctColorForFirstSlot)
-        {
-            FirstPuzzle = true;
-            Debug.Log("Correct color placed in the first slot");
+            MainManager.Slots[slot] = true;
         }
         else
         {
-            
-            Debug.Log("Incorrect color for the first slot");
+            MainManager.Slots[slot] = false;
         }
     }
-
-    private void HandleSecondSlot(Collider2D collision)
-    {
-        string enteredColor = collision.transform.tag;
-
-        if (enteredColor == correctColorForSecondSlot)
-        {
-            SecondPuzzle = true;
-            Debug.Log("Correct color placed in the second slot");
-        }
-        else
-        {
-           
-            Debug.Log("Incorrect color for the second slot");
-        }
-    }
-
-    private void HandleThirdSlot(Collider2D collision)
-    {
-        string enteredColor = collision.transform.tag;
-
-        if (enteredColor == correctColorForThirdSlot)
-        {
-            ThirdPuzzle = true;
-            Debug.Log("Correct color placed in the third slot");
-        }
-        else
-        {
-           
-            Debug.Log("Incorrect color for the third slot");
-        }
-    }
-
 }
-
-
-
-
-
-
